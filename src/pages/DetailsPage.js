0fa8acc2c1e424f51d6c72cca6d203db05f7b8fc
alexis <alexis.corbacho@hotmail.com>
2019-11-15 18:44:57 +0100

    bug solved

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -10,15 +10,17 @@ import PokemonDetailBox from '../components/contentBoxes/PokemonDetailBox'
 class DetailsPage extends Component {
 
     componentDidMount() {
-        const { name } = this.props.match.params
+        const { name = '' } = this.props.match.params
         this.props.fetchPokemon(name)
     }
 
     render() {
+        const { name = '' } = this.props.match.params
+
         return (
             <section id="detailsPage" >
                 {
-                    this.props.pokemon.name
+                    this.props.pokemon.name == name
                         ?
                         <Fragment>
                             <PokemonDetailBox pokemon={this.props.pokemon} />
